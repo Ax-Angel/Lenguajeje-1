e64eb231f0dab7c0f737ccabdc110dc7035da13d
Mateo <mateo.atr@gmail.com>
2017-10-29 09:36:58 -0600

    Store diff files in folders; remove EOF bug

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SOURCE=token.hpp token.cpp lexer.hpp lexer.cpp tablas.hpp tablas.cpp main.cpp
+SOURCE=src/token.hpp src/token.cpp src/lexer.hpp src/lexer.cpp src/tablas.hpp src/tablas.cpp src/main.cpp
 MYPROGRAM=anaLex
 
 CC=g++
---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+/*
+	Analizador Léxico
+	Creado por: Mateo Torres Ruiz y Ángel Rodríguez Mendoza
+	Proyecto para la asignatura de Compiladores 2018-1 Grupo 2
+ */
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+
+#include "lexer.hpp"
+
+//Se crea la instancia de las tablas.
+Tabla* tabla = Tabla::obtenerInstancia();
+
+void sanitize (char* file) {
+  std::ofstream out(file, std::ios::app);
+
+  std::fstream check(file);
+  if (check.seekg(-1, std::ios_base::end).get() != '\n') {
+    out << '\n';
+  }
+
+  out.close();
+}
+
+int main (int argc, char* argv[]) {
+  sanitize(argv[1]);
+  std::ifstream file(argv[1]);
+  std::istream &entrada(file);
+  
+  std::shared_ptr<Lexer> lexer(std::make_shared<Lexer>(entrada));
+
+  printf("Hello\n");
+  int i = 0;
+  
+  while (true) {
+    auto &t = *lexer -> obtenerSiguienteToken();
+
+    if(entrada.peek() == EOF){
+      break;
+    }
+
+    tabla -> agregarToken(t);
+    printf("%d ", i++);
+  }
+  
+  tabla -> imprimeIdentificador();
+  tabla -> imprimeCadenaConstante();
+  tabla -> imprimeTablaTokens();
+
+  return 0;
+}
